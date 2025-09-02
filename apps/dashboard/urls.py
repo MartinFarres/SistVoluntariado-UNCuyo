074b5e2f91c48d5f6f4c38c974f652b20ef55e43
FrancoYudica <francoyudica@gmail.com>
2025-09-02 10:02:56 -0300

    Persona creation form

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import persona_list
+from .views import persona_list, persona_create
 
 urlpatterns = [
     path("", persona_list, name="persona_list"),
+    path("persona/create", persona_create, name="persona_create"),
 ]
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from .models import Persona
+from .forms import PersonaForm
+from django.shortcuts import redirect
+from django.urls import reverse
 
 def persona_list(request):
     personas = Persona.objects.all()
     return render(request, 'dashboard/personasTable.html', {'personas': personas})
+
+
+def persona_create(request):
+    if request.method == 'POST':
+        form = PersonaForm(request.POST)
+        
+        if form.is_valid():
+            dni = form.cleaned_data['dni']
+            name = form.cleaned_data['name']
+            surname = form.cleaned_data['surname']
+            persona = Persona()
+            persona.dni = dni
+            persona.name = name
+            persona.surname = surname
+            persona.save()
+
+            return redirect(reverse('persona_list'))
+    else:
+        form = PersonaForm()
+    return render(request, 'dashboard/persona/form.html', {'form': form})
