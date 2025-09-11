c481cc81815450ffc565a09f7e4f2ae55f7729e7
FrancoYudica <francoyudica@gmail.com>
2025-09-11 18:28:21 -0300

    Validaciones ubicacion

--- voluntariados-backend/apps/ubicacion/serializers.py
+++ voluntariados-backend/apps/ubicacion/serializers.py
@@ -6,6 +6,16 @@ class PaisSerializer(serializers.ModelSerializer):
         model = Pais
         fields = "__all__"
 
+        extra_kwargs = {
+            "nombre": {"required": True, "allow_null": False, "allow_blank": False},
+            "codigo": {"required": True, "allow_null": False, "allow_blank": False},
+        }
+
+    def validate_nombre(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("El nombre solo puede contener letras")
+        return value
+
 class ProvinciaSerializer(serializers.ModelSerializer):
     pais = PaisSerializer(read_only=True)
     pais_id = serializers.PrimaryKeyRelatedField(source="pais", queryset=Pais.objects.all(), write_only=True)
@@ -13,6 +23,16 @@ class ProvinciaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Provincia
         fields = "__all__"
+
+        extra_kwargs = {
+            "nombre": {"required": True, "allow_null": False, "allow_blank": False},
+            "pais": {"required": True, "allow_null": False},
+        }
+
+    def validate_nombre(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("El nombre solo puede contener letras")
+        return value
 
 class DepartamentoSerializer(serializers.ModelSerializer):
     provincia = ProvinciaSerializer(read_only=True)
@@ -23,6 +43,16 @@ class DepartamentoSerializer(serializers.ModelSerializer):
         fields = "__all__"
         read_only_fields = ("id",)
 
+        extra_kwargs = {
+            "nombre": {"required": True, "allow_null": False, "allow_blank": False},
+            "provincia": {"required": True, "allow_null": False},
+        }
+
+    def validate_nombre(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("El nombre solo puede contener letras")
+        return value
+
 class LocalidadSerializer(serializers.ModelSerializer):
     departamento = DepartamentoSerializer(read_only=True)
     departamento_id = serializers.PrimaryKeyRelatedField(source="departamento", queryset=Departamento.objects.all(), write_only=True)
@@ -31,5 +61,13 @@ class LocalidadSerializer(serializers.ModelSerializer):
         model = Localidad
         fields = "__all__"
         read_only_fields = ("id",)
-    
-    
+
+        extra_kwargs = {
+            "nombre": {"required": True, "allow_null": False, "allow_blank": False},
+            "departamento": {"required": True, "allow_null": False},
+        }
+        
+    def validate_nombre(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("El nombre solo puede contener letras")
+        return value
