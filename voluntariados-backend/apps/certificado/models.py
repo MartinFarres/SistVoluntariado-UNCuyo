331458abf4755578a96e1f0ba586293f39071499
MauroSorbello <sorbello.mauro.em.cn@gmail.com>
2025-09-27 15:45:45 -0300

    Certificado y autoridad model

--- voluntariados-backend/apps/certificado/models.py
+++ voluntariados-backend/apps/certificado/models.py
@@ -1,11 +1,24 @@
+import os
 from django.db import models
+
+from apps.asistencia.models import Asistencia
 from apps.soft_delete.model import SoftDeleteModel
 
-class Certificado(SoftDeleteModel):
-    voluntario = models.ForeignKey("persona.Voluntario", on_delete=models.CASCADE, related_name="certificados")
-    voluntariado = models.ForeignKey("voluntariado.Voluntariado", null=True, blank=True, on_delete=models.SET_NULL)
-    horas = models.DecimalField(max_digits=6, decimal_places=2, default=0)
-    fecha_emision = models.DateField(auto_now_add=True)
+def authority_signature_path(instance, filename):
+    nombre = instance.nombre
+    apellido = instance.apellido
+    cargo = instance.cargo
+    _, extension = os.path.splitext(filename)
+    return f'firmas/firma-{nombre}-{apellido}-{cargo}{extension}'
 
-    def __str__(self):
-        return f"Certificado {self.voluntario} - {self.horas}h"
+class Autoridad(SoftDeleteModel):
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    cargo = models.CharField(max_length=150)
+    entidad_encargada = models.CharField(max_length=150)
+    firma = models.FileField(upload_to=authority_signature_path, null=True, blank=True)
+
+class Certificado(SoftDeleteModel):
+    asistencia = models.ForeignKey(Asistencia, on_delete=models.SET_NULL, null=True,  related_name='asistencia')
+    archivo = models.FileField(upload_to='certificado/%Y/%m/%d', null=True, blank=True)
+    autoridades = models.ManyToManyField('Autoridad', related_name='certificados')
