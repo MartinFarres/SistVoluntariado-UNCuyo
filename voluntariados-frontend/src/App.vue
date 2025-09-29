c0c7f39714631bfbf1cb90c2e5ff9e154f3485f3
MartinFarres <martinfarres@gmail.com>
2025-09-29 17:50:22 -0300

    basic front structure

---
+++ voluntariados-frontend/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" src="./assets/logo.svg" width="125" height="125" />
+    <div class="wrapper">
+      <!-- <HelloWorld msg="¡Bienvenido a tu aplicación Vue 3!" /> -->
+      <nav>
+        <RouterLink to="/">Inicio</RouterLink>
+        <RouterLink to="/about">Acerca de</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+/* Más estilos... */
+</style>
